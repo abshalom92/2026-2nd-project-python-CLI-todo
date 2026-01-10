8401f7507e47ea4b5b83ef65d146996dd4786fee
abshalom92 <abshalom92@gmail.com>
2026-01-09 23:36:51 -0500

    Initial Project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+#Python CLI To-Do App
+
+A simple command-line to-do application written in Python.
---
+++ todo.py
@@ -0,0 +1,5 @@
+def main():
+    print("Welcome to the Python CLI To-Do App")
+
+if __name__== "__main__":
+    main()
