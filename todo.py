23622e2a11cb12b41c33ac84b7cb21505f7fce0e
abshalom92 <abshalom92@gmail.com>
2026-01-14 00:13:28 -0500

    Finished argparse functionality

--- todo.py
+++ todo.py
@@ -1,41 +1,49 @@
-import sys
+import argparse
 from storage import load_tasks
 from commands import list_tasks, add_task, complete_task
 
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description = "Simple command-line to-do application"
+    )
+
+    subparsers = parser.add_subparsers(dest = "command", required = True)
+
+    #list command
+
+    subparsers.add_parser("list", help="List all tasks")
+
+    #add command
+
+    add_parser = subparsers.add_parser("add", help="Add a task")
+    add_parser.add_argument("text", nargs = "+", help="Task description")
+
+    #complete command
+
+    complete_parser = subparsers.add_parser(
+        "complete", help="Mark a task as complete"
+    )
+    complete_parser.add_argument(
+        "task_number", type=int, help="Task number to complete"
+    )
+
+    return parser
 
 def main():
+    parser = create_parser()
+    args = parser.parse_args()
+
     tasks = load_tasks()
 
-    if len(sys.argv) < 2:
+    if args.command == "list":
         list_tasks(tasks)
-        return
 
-    command = sys.argv[1]
-
-    if command == "add":
-        if len(sys.argv) < 3:
-            print('Usage: python todo.py add "Task description"')
-            return
-
-        task_text = " ".join(sys.argv[2:])
+    elif args.command == "add":
+        task_text = " ".join(args.text)
         add_task(tasks, task_text)
 
-    elif command == "complete":
-        if len(sys.argv) != 3:
-            print("Usage: python todo.py complete <task_number>")
-            return
-
-        try:
-            task_number = int(sys.argv[2])
-        except ValueError:
-            print("Task number must be an integer.")
-            return
-
-        complete_task(tasks, task_number)
-
-    else:
-        print(f"Unknown command: {command}")
-
+    elif args.command == "complete":
+        complete_task(tasks, args.task_number)
 
 if __name__ == "__main__":
     main()
